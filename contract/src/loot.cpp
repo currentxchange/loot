b4a98b1cf95de0ab9105facbbecada0db24fc223
Douglas Butner <dougbutner@gmail.com>
2024-01-15 17:59:11 -0600

    CHANGE finalized parameter change for rmtemplate and addtemplate

--- contract/src/loot.cpp
+++ contract/src/loot.cpp
@@ -56,38 +56,38 @@ ACTION loot::addtemplates(const int32_t& template_id, const name& collection, co
 
 
     // check if the hourly rate is valid
-    check(t.timeunit_rate.amount > 0, "timeunit_rate must be positive");
-    check(config.token_symbol == t.timeunit_rate.symbol, "symbol mismatch");
+    check(timeunit_rate.amount > 0, "timeunit_rate must be positive");
+    check(config.token_symbol == timeunit_rate.symbol, "symbol mismatch");
 
     // check if the template exists in atomicassets and it's valid
-    const auto& aa_template_tbl = atomicassets::get_templates(t.collection);
+    const auto& aa_template_tbl = atomicassets::get_templates(collection);
 
-    const auto& aa_template_itr = aa_template_tbl.find(uint64_t(t.template_id));
+    const auto& aa_template_itr = aa_template_tbl.find(uint64_t(template_id));
 
     if (aa_template_itr == aa_template_tbl.end()) {
-        check(false, string("template (" + to_string(t.template_id) + ") not found in collection " + t.collection.to_string()).c_str());
+        check(false, string("template (" + to_string(template_id) + ") not found in collection " + collection.to_string()).c_str());
     }
 
-    const auto& template_row = template_tbl.find(uint64_t(t.template_id));
+    const auto& template_row = template_tbl.find(uint64_t(template_id));
 
     // insert the new template or update it if it already exists
     if (template_row == template_tbl.end()) {
         template_tbl.emplace(get_self(), [&](template_s& row) {
-            row.template_id = t.template_id;
-            row.collection = t.collection;
-            row.timeunit_rate = t.timeunit_rate;
+            row.template_id = template_id;
+            row.collection = collection;
+            row.timeunit_rate = timeunit_rate;
         });
     } else {
         template_tbl.modify(template_row, get_self(), [&](template_s& row) {
-            row.template_id = t.template_id;
-            row.collection = t.collection;
-            row.timeunit_rate = t.timeunit_rate;
+            row.template_id = template_id;
+            row.collection = collection;
+            row.timeunit_rate = timeunit_rate;
         });
     }
 
 }
 
-ACTION loot::rmvtemplates(const int32_t& template_id, const name& collection, const asset& timeunit_rate)
+ACTION loot:: rmtemplates(const int32_t& template_id, const name& collection, const asset& timeunit_rate)
 {
     // check contract auth
     check(has_auth(get_self()), "this action is admin only");
@@ -99,7 +99,7 @@ ACTION loot::rmvtemplates(const int32_t& template_id, const name& collection, co
     template_t template_tbl(get_self(), get_self().value);
 
 
-    const auto& template_row = template_tbl.find(uint64_t(t.template_id));
+    const auto& template_row = template_tbl.find(uint64_t(template_id));
 
     // erase the template if it already exists
     if (template_row != template_tbl.end()) {
